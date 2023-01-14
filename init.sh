b67a5db42cf60b39cfbbd4ad2c18cce95a939d4a
Link <linkec@live.com>
2023-01-14 18:28:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@ echo "net.core.default_qdisc=fq">>/etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr">>/etc/sysctl.conf
 sysctl -p
 apt update
+rm -rf /etc/haproxy/haproxy.cfg
 apt install -y haproxy
 curl https://raw.githubusercontent.com/RayP2P/test/main/haproxy.cfg>/etc/haproxy/haproxy.cfg
 systemctl restart haproxy
+ps -ef | grep haproxy
+lsof -i :8443
